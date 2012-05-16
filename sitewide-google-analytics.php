dc4761ae2043e13db5326fc274bef1b17313daa9
Mike Weber <msweber@gmail.com>
2012-05-16 15:48:22 -0700

    Added WordPress plugin comment.

--- sitewide-google-analytics.php
+++ sitewide-google-analytics.php
@@ -1,4 +1,29 @@
 <?php
+/*
+Plugin Name: Sitewide Google Analytics 
+Plugin URI: https://github.com/weberwithoneb/sitewide-google-analytics
+Description: Installs the same Google Analytics code across all sites.
+Version: 0
+Author: Mike Weber (weberwithoneb)
+Author URI: https://github.com/weberwithoneb
+License: GPL2
+*/
+/*  Copyright 2012  Mike Weber  (email : msweber@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 class Msw_Google_Analytics {
 	
